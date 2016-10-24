2c5ba45f2a6bad8950ee1221900c24fb72b58378
MilesDuronCIMAT <milesduron@uaz.edu.mx>
2016-10-23 20:08:19 -0500

    update fabfile

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -2,7 +2,7 @@ from fabric.contrib.files import append, exists, sed
 from fabric.api import env, local, run 
 import random
 
-REPO_URL = 'https://github.com/MilesDuronCIMAT/current_excersise.git'
+REPO_URL = 'https://github.com/MilesDuronCIMAT/current_exercise'
 
 def deploy():
 	site_folder = '/home/%s/sites/%s' % (env.user, env.host)
