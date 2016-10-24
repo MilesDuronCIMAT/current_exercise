740f80671500131af9787045960cfb9ab8ef7766
MilesDuronCIMAT <milesduron@uaz.edu.mx>
2016-10-24 01:23:18 -0500

    Adjust new list view to do model validation

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from lists.models import Item, List
+from django.core.exceptions import ValidationError
 
 class ListAndItemModelsTests(TestCase):
     def test_saving_and_retrieving_items(self):
