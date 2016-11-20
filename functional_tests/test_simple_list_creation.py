bf654003ad73bc204302651755bf767c7a95b239
MilesDuronCIMAT <milesduron@uaz.edu.mx>
2016-11-20 14:28:54 -0600

    Changes in yml and functional tests to pass in travis

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -53,7 +53,7 @@ class NewVisitorTest(FunctionalTest):
         ## We use a new browser session to make sure that no information
         ## of Edith's is coming through from cookies etc
         self.browser.quit()
-        self.browser = webdriver.Chrome('/home/miles/ejercicios_libro/chromedriver')
+        self.browser = webdriver.Chrome('/usr/bin/chromedriver')
 
         # Francis visits the home page.  There is no sign of Edith's
         # list 
